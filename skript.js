16f53f6333ebc07636a2a2eb2c3521fba5316c60
martyasbutkus <noobtuberis1990@gmail.com>
2023-11-19 14:45:08 +0200

    init

---
+++ skript.js
@@ -0,0 +1,18 @@
+//Ciklu 1 Uzduotis
+for (var i = 0; i < 10; i++){
+    console.log("Martynas")
+}
+
+
+//Ciklu 2 Uzduotis
+var fname = 'Martynas'
+var kiek = 10
+for (var i = 0; i < kiek; i++){
+    console.log(fname)
+}
+
+//Ciklu 3 Uzduotis
+var vardas = "Martynas"
+for (var i = 10; i >= 1; i--){
+    console.log(i +"." + vardas)
+}
